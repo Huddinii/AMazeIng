12edd72da005d8b1a0116f89190671f5e597e08d
fstadler <fstadler@student.42heilbronn.de>
2026-08-17 17:52:09 +0200

    added class TColor with logic to go through list and increment so every function call gives diff color, adjusted player_interaction and a_maxe_ing for testing purposes

--- GUI.py
+++ GUI.py
@@ -1,8 +1,23 @@
 from configparser import ConfigParser
+from termcolor import colored
+
+
+class TColor:
+    def __init__(self):
+        self.colors = ['green', 'red', 'yellow', 'blue', 'white']
+        self.i = 0
+
+    def __call__(self) -> None:
+        text = colored('┤', self.colors[self.i])
+        print(text)
+        self.i += 1
+
+
+t_color = TColor()
 
 
 def player_interaction() -> None:
-    # need actual functions to do the stuff instread of just print, only placeholder
+    # need actual functions to do the stuff instread of just print
     input_str: str = input("type number 1-4: ")
     match input_str:
         case "1":
@@ -11,6 +26,7 @@ def player_interaction() -> None:
             print("Show/Hide path from entry to exit")
         case "3":
             print("Rotate maze color")
+            t_color()
         case "4":
             print("Quit")
             exit(0)
@@ -19,3 +35,7 @@ def player_interaction() -> None:
 def config_read() -> None:
     config = ConfigParser()
     config.read("config.txt")
+
+
+def draw_maze(maze_parts: list[str]) -> None:
+    pass
--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,5 +1,10 @@
 import GUI
+# from mazegen import MazeGenerator
 
 
 if __name__ == '__main__':
+    GUI.player_interaction()
+    GUI.player_interaction()
+    GUI.player_interaction()
+    GUI.player_interaction()
     GUI.player_interaction()
