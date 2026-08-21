4c8face95e6e3cc5fc059cfa21cf2fcd5adfe86a
Luca Hutter <lhutter@3-G-1.42heilbronn.de>
2026-08-21 16:07:03 +0200

    finished bonus, fixed mypy

--- mazegen/mazegen.py
+++ mazegen/mazegen.py
@@ -10,14 +10,14 @@ class Wall(Enum):
 
 
 class Cell:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int) -> None:
         self.walls = [Wall.NORTH, Wall.EAST, Wall.SOUTH, Wall.WEST]
         self.position = (x, y)
         self.checked = False
         self.logo = False
         self.traveled = False
 
-    def out(self):
+    def out(self) -> int:
         out = 0
         for wall in self.walls:
             out += 2 ** wall.value
@@ -25,13 +25,13 @@ class Cell:
 
 
 class Maze:
-    def __init__(self, height: int, width: int):
+    def __init__(self, height: int, width: int) -> None:
         self.height = height
         self.width = width
         self.cells = [[Cell(x, y) for x in range(height)]for y in range(width)]
-        self.path = None
+        self.path: str | None = None
 
-    def create_logo(self):
+    def create_logo(self) -> None:
         if self.height > 9 and self.width > 9:
             middle = (int(self.height / 2), int(self.width / 2))
             start = (middle[0] - 2, middle[1] - 3)
@@ -51,24 +51,12 @@ class Maze:
             self.cells[middle[0] + 1][middle[1] + 2].logo = False
             self.cells[middle[0] + 1][middle[1] + 3].logo = False
 
-    def dfs(self, x: int, y: int):
+    def dfs(self, x: int, y: int) -> None:
         self.cells[x][y].checked = True
-        wall = {
-                Wall.NORTH: (x - 1, y),
-                Wall.EAST: (x, y + 1),
-                Wall.SOUTH: (x + 1, y),
-                Wall.WEST: (x, y - 1)
-                }
-        oposite = {
-                Wall.NORTH: Wall.SOUTH,
-                Wall.EAST: Wall.WEST,
-                Wall.SOUTH: Wall.NORTH,
-                Wall.WEST: Wall.EAST
-                }
         to_check = [Wall.NORTH, Wall.EAST, Wall.SOUTH, Wall.WEST]
         while len(to_check) > 0:
             check = random.choice(to_check)
-            i, j = wall[check]
+            i, j = self.get_cell(x, y, check)
             if (i < 0 or i >= self.height or j < 0 or j >= self.width):
                 to_check.remove(check)
             elif self.cells[i][j].checked is True:
@@ -77,45 +65,63 @@ class Maze:
                 to_check.remove(check)
             else:
                 self.cells[x][y].walls.remove(check)
-                self.cells[i][j].walls.remove(oposite[check])
+                self.cells[i][j].walls.remove(self.get_opposite(check))
                 self.dfs(i, j)
                 to_check.remove(check)
 
-    def prim(self):
+    def prim(self) -> None:
         x, y = 0, 0
         cur = (0, 0)
         self.cells[x][y].checked = True
+        walls = []
+        for w in self.cells[x][y].walls:
+            dir = self.get_cell(x, y, w)
+            if (dir[0] >= 0 and dir[0] < self.height and
+                    dir[1] >= 0 and dir[1] < self.width):
+                walls.append({cur: w, dir: self.get_opposite(w)})
+        while len(walls) > 0:
+            choice = random.choice(walls)
+            c1, c2 = choice.keys()
+            c1_check = (self.cells[c1[0]][c1[1]].checked ^
+                        self.cells[c1[0]][c1[1]].logo)
+            c2_check = (self.cells[c2[0]][c2[1]].checked ^
+                        self.cells[c2[0]][c2[1]].logo)
+            if ((c1_check is False and c2_check is True)
+                    or (c1_check is True and c2_check is False)):
+                if not c1_check:
+                    next = c1
+                elif not c2_check:
+                    next = c2
+                self.remove_walls(choice)
+                self.cells[next[0]][next[1]].checked = True
+                for w in self.cells[next[0]][next[1]].walls:
+                    dir = self.get_cell(next[0], next[1], w)
+                    if (dir[0] >= 0 and dir[0] < self.height and
+                            dir[1] >= 0 and dir[1] < self.width):
+                        walls.append({next: w, dir: self.get_opposite(w)})
+            walls.remove(choice)
+
+    def remove_walls(self, walls: dict[tuple[int, int], Wall]) -> None:
+        for key in walls.keys():
+            self.cells[key[0]][key[1]].walls.remove(walls[key])
+
+    def get_cell(self, x: int, y: int, k: Wall) -> tuple[int, int]:
         wall = {
                 Wall.NORTH: (x - 1, y),
                 Wall.EAST: (x, y + 1),
                 Wall.SOUTH: (x + 1, y),
-                Wall.WEST: (x, y - 1)
-                }
-        oposite = {
+                Wall.WEST: (x, y - 1)}
+        return wall[k]
+
+    def get_opposite(self, k: Wall) -> Wall:
+        opposite = {
                 Wall.NORTH: Wall.SOUTH,
                 Wall.EAST: Wall.WEST,
                 Wall.SOUTH: Wall.NORTH,
-                Wall.WEST: Wall.EAST
-                }
-        walls = []
-        for w in self.cells[x][y].walls:
-            if (wall[w][0] >= 0 and wall[w][0] < self.height and
-                    wall[w][1] >= 0 and wall[w][1] < self.width):
-                walls.append({cur: w, wall[w]: oposite[w]})
-        print(walls)
-        while len(walls) > 0:
-            choice = random.choice(walls)
-            c1, c2 = choice.keys()
-            print(c1, c2)
-            if (self.cells[c1[0]][c1[1]].checked is False or
-                    self.cells[c2[0]][c2[1]].checked is False):
-                #   remove cur wall and next wall
-                #   add valid walls of next to list
-                #   set next checked to True
-                print(c1, c2)
-            walls.remove(choice)
+                Wall.WEST: Wall.EAST}
+        return opposite[k]
 
-    def solve_maze(self, start: tuple[int, int], end: tuple[int, int]):
+    def solve_maze(self, start: tuple[int, int], end: tuple[int, int]) -> None:
         q = []
         self.cells[start[0]][start[1]].traveled = True
         q.append(Node(start))
@@ -123,23 +129,19 @@ class Maze:
         while len(q) > 0:
             n = q[0]
             q.pop(0)
-            dir = {
-                    Wall.NORTH: (n.coords[0] - 1, n.coords[1]),
-                    Wall.EAST: (n.coords[0], n.coords[1] + 1),
-                    Wall.SOUTH: (n.coords[0] + 1, n.coords[1]),
-                    Wall.WEST: (n.coords[0], n.coords[1] - 1),
-                    }
             if n.coords == end:
                 out = ""
                 while n.parent is not None:
-                    out += n.dir
+                    out = out + n.dir
                     n = n.parent
                 self.path = out[::-1]
+                return
             for wall in [x for x in walls if x not in
                          self.cells[n.coords[0]][n.coords[1]].walls]:
-                if self.cells[dir[wall][0]][dir[wall][1]].traveled is False:
-                    self.cells[dir[wall][0]][dir[wall][1]].traveled = True
-                    w = Node(dir[wall])
+                dir = self.get_cell(n.coords[0], n.coords[1], wall)
+                if self.cells[dir[0]][dir[1]].traveled is False:
+                    self.cells[dir[0]][dir[1]].traveled = True
+                    w = Node(dir)
                     w.dir = wall.name[0]
                     w.parent = n
                     q.append(w)
@@ -152,17 +154,18 @@ class Maze:
 
 
 class Node:
-    def __init__(self, coords: tuple[int, int]):
+    def __init__(self, coords: tuple[int, int]) -> None:
         self.coords = coords
-        self.parent = None
-        self.dir = None
+        self.parent: Node | None = None
+        self.dir: str = ""
 
 
 class MazeGenerator:
     maze: Maze
 
     def __init__(self, width: int, height: int, start: tuple[int, int],
-                 ex: tuple[int, int], perfect: bool = True, sort: str = "DFS"):
+                 ex: tuple[int, int], perfect: bool = True,
+                 sort: str = "DFS") -> None:
         self.width = width
         self.height = height
         self.start = start
@@ -184,7 +187,7 @@ class MazeGenerator:
     def set_seed(self, seed: int) -> None:
         random.seed(seed)
 
-    def out(self, output: str | None = None):
+    def out(self, output: str | None = None) -> None:
         out_str = ""
         for x in range(self.height):
             for y in range(self.width):
@@ -205,7 +208,7 @@ class MazeGenerator:
 
 
 if __name__ == "__main__":
-    mazegen = MazeGenerator(11, 11, (0, 0), (9, 9), perfect=False, sort="PRIM")
+    mazegen = MazeGenerator(11, 11, (0, 0), (9, 9), sort="PRIM")
     mazegen.set_seed(1000)
     mazegen.generate_maze()
-    mazegen.out()
+    mazegen.out("output.txt")
